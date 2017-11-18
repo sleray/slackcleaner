cef01707366d85e64e80ac4f248b64da970b5ced
sleray <syleray@gmail.com>
2017-11-18 11:29:37 +0100

    Initial commit

---
+++ src/com/slackcleaner/launch/Launcher.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.slackcleaner.launch;
+
+/**
+ * @author sleray
+ *
+ */
+public class Launcher {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+
+		Clean clean = new Clean();
+		clean.processClean(0);
+
+		
+		
+		
+	}
+
+}
